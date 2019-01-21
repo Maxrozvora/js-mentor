6552a5b731008dc6566dfe662bee76dd9b54d3a4
Maxrozvora <rozvora.max@gmail.com>
2019-01-21 22:07:07 +0200

    work (index.js) Закометировал функции для следуюшего задания

--- index.js
+++ index.js
@@ -20,35 +20,35 @@ const todos = [
 ]
 
 // Render todos
-const renderTodos = (todos) => {
-    document.querySelector('#todo-list').innerHTML = '';
-    todos.forEach(todo => {
-        document.querySelector('#todo-list').appendChild(generateDom(todo))
-    })
-}
+// const renderTodos = (todos) => {
+//     document.querySelector('#todo-list').innerHTML = '';
+//     todos.forEach(todo => {
+//         document.querySelector('#todo-list').appendChild(generateDom(todo))
+//     })
+// }
 
 // Generate DOM Element for an individual todo
-const generateDom = (todo) => {
-    const newTodo = document.createElement('li')
-    newTodo.className = 'list-group-item'
-    newTodo.innerHTML = `<div class="d-flex justify-content-between">
-                        <div class="ml-3">
-                            <input class="form-check-input" type="checkbox">
-                            <span>${todo.text}</span>
-                            <div class="small text-secondary">${todo.date}</div>
-                        </div>
-                        <div>
-                            <button class="btn btn-sm btn-secondary">
-                                <i class="fa fa-pencil" aria-hidden="true"></i>
-                            </button>
-                            <button class="btn btn-sm btn-secondary">
-                                <i class="fa fa-trash-o" aria-hidden="true"></i>
-                            </button>
-                        </div>
-                    </div>`
-
-    return newTodo
-}
+// const generateDom = (todo) => {
+//     const newTodo = document.createElement('li')
+//     newTodo.className = 'list-group-item'
+//     newTodo.innerHTML = `<div class="d-flex justify-content-between">
+//                         <div class="ml-3">
+//                             <input class="form-check-input" type="checkbox">
+//                             <span>${todo.text}</span>
+//                             <div class="small text-secondary">${todo.date}</div>
+//                         </div>
+//                         <div>
+//                             <button class="btn btn-sm btn-secondary">
+//                                 <i class="fa fa-pencil" aria-hidden="true"></i>
+//                             </button>
+//                             <button class="btn btn-sm btn-secondary">
+//                                 <i class="fa fa-trash-o" aria-hidden="true"></i>
+//                             </button>
+//                         </div>
+//                     </div>`
+//
+//     return newTodo
+// }
 
 // Create new todo item
 document.querySelector('#add-form').addEventListener('submit', function (e) {
@@ -66,18 +66,9 @@ document.querySelector('#add-form').addEventListener('submit', function (e) {
     renderTodos(todos)
 })
 
-// Create complete todo
-document.querySelector('#todo-list').addEventListener('click', function (e) {
-
-    if (e.target.tagName === 'LI') {
-        e.target.checked = !e.target.checked
-        console.log(e); // TODO console.log
-    }
-})
-
 // Generate random id
 function doId() {
     return Math.random().toString(36).substr(2, 16)
 }
 
-renderTodos(todos)
+// renderTodos(todos)
