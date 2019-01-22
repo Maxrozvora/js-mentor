e4f638a8a8734a0e71596055b8e3e85f08b10232
Maxrozvora <rozvora.max@gmail.com>
2019-01-22 22:54:01 +0200

    work (index.html) Удалил закоментированой код

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 const todos = getTodos()
 
-
-// Render todos
-// const renderTodos = (todos) => {
-//     document.querySelector('#todo-list').innerHTML = '';
-//     todos.forEach(todo => {
-//         document.querySelector('#todo-list').appendChild(generateDom(todo))
-//     })
-// }
-
-// Generate DOM Element for an individual todo
-// const generateDom = (todo) => {
-//     const newTodo = document.createElement('li')
-//     newTodo.className = 'list-group-item'
-//     newTodo.innerHTML = `<div class="d-flex justify-content-between">
-//                         <div class="ml-3">
-//                             <input class="form-check-input" type="checkbox">
-//                             <span>${todo.text}</span>
-//                             <div class="small text-secondary">${todo.date}</div>
-//                         </div>
-//                         <div>
-//                             <button class="btn btn-sm btn-secondary">
-//                                 <i class="fa fa-pencil" aria-hidden="true"></i>
-//                             </button>
-//                             <button class="btn btn-sm btn-secondary">
-//                                 <i class="fa fa-trash-o" aria-hidden="true"></i>
-//                             </button>
-//                         </div>
-//                     </div>`
-//
-//     return newTodo
-// }
-
 // Create new todo item
 document.querySelector('#add-form').addEventListener('submit', function (e) {
     e.preventDefault()
