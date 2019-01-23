2bbf2bad46bb76785f0fbec2fd2f8763e5eaf5c0
Maxrozvora <rozvora.max@gmail.com>
2019-01-23 22:59:24 +0200

    work (index.js) Реализация вывода таска

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ document.querySelector('#add-form').addEventListener('submit', function (e) {
         completed: false,
         date,
         id: generateId()
-    }
+    };
     todos.push(todo);
     setTodos(todos);
     resetAddTodoForm(e);
-    console.log(getTodos()); // TODO console.log
+    displayTodosOnScreen(todos);
 })
 
 // Clean form inputs
@@ -42,7 +42,43 @@ function getTodos() {
         return [];
     }
 }
+// Display list todos
+function displayTodosOnScreen(todos) {
+    const todoList = document.querySelector('#todo-list');
+    todoList.innerHTML = '';
+    todos.forEach(todo => {
+        todoList.prepend(generateTodo(todo));
+    })
+}
 
-console.log(getTodos()); // TODO console.log
+// Generate item of todo
+function generateTodo(todo) {
+    const newTodo = document.createElement('li');
+    newTodo.className = 'list-group-item d-flex justify-content-between';
+    // Create el for text todo
+    const todoText = document.createElement('div');
+    todoText.className = 'ml-3';
+    todoText.innerHTML = ` <input class="form-check-input" id="${todo.id}" type="checkbox">
+                            <label for="${todo.id}">${todo.text}</label>
+                            <div class="small text-secondary">${todo.date}</div>`;
+    // Create wrapper for buttons
+    const btnGroup = document.createElement('div');
+    // Create btn for edit todo
+    const btnEdit = document.createElement('button');
+    btnEdit.className = 'btn btn-sm btn-secondary mr-1';
+    btnEdit.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i>';
+    // Create btn for delete todo
+    const btnDelete = document.createElement('button');
+    btnDelete.className = 'btn btn-sm btn-secondary';
+    btnDelete.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>';
+    // Add element to the li parent tag
+    btnGroup.appendChild(btnEdit);
+    btnGroup.appendChild(btnDelete);
 
-// renderTodos(todos)
+    newTodo.appendChild(todoText);
+    newTodo.appendChild(btnGroup);
+
+    return newTodo;
+}
+
+displayTodosOnScreen(todos);
