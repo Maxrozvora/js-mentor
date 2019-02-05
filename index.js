1d6220f858c61f7fc9cc4d8dcad33d2e9129456b
Maxrozvora <rozvora.max@gmail.com>
2019-02-05 22:26:08 +0200

    work (index.js) Попытка реализации сортировки

--- index.js
+++ index.js
@@ -59,51 +59,58 @@ function displayTodos(todos) {
     console.log(todos); // TODO console.log
     const template = document.querySelector('#todo-item').innerHTML;
     Mustache.parse(template);
-    document.querySelector('#todo-list2').innerHTML = Mustache.render(template, todos);
+    // document.querySelector('#todo-list2').innerHTML = Mustache.render(template, sortTodos(todos, sortMethod));
+    document.querySelector('#todo-list2').innerHTML = Mustache.render(template, sortTodos(todos, 'date', 1));
 }
 
 document.querySelector('.dropdown-menu').addEventListener('click', function (e) {    
     const sortMethod = e.target.dataset.sort;
-    let sortArr = sortTodos(todos, sortMethod);
+    const sortOrder = e.target.dataset.order === 'asc' ? -1 : 1;
+    let sortArr = sortTodos(todos, sortMethod, sortOrder);
     displayTodos(sortArr)
 });
 
 //Sort todos
-function sortTodos(todos, sortMethod) {
+function sortTodos(todos, sortMethod, sortOrder) {
+
     switch (sortMethod) {
         // Sort by date
         case 'date':
              const sortArrByDate = todos.todos.sort((a, b) => {
                  const dateA = new Date(a.date);
                  const dateB = new Date(b.date);
-                return (dateA - dateB);
+                return (dateA - dateB) * sortOrder;
             });
-            return returnResultSort(sortArrByDate);
+            return {
+                todos: sortArrByDate
+            };
         // Sort by completed
-        case 'done':
+        case 'complete':
             const sortArrByDone = todos.todos.sort((a,b) => {
-                return b.complete - a.complete;
+                return (b.complete - a.complete) * sortOrder;
             });
-            return returnResultSort(sortArrByDone);
+            return {
+                todos: sortArrByDone
+            };
         // Sort by name
-        case 'name':
+        case 'text':
+            console.log(sortOrder); // TODO console.log
             const sortArrByName =  todos.todos.sort((a,b) => {
                 if (a.text > b.text) {
-                    return 1; }
+                    return 1 * sortOrder;
+                }
                 if (a.text < b.text) {
-                    return -1; }
+                    return -1 * sortOrder;
+                }
                 return 0;
             });
-            return returnResultSort(sortArrByName);
+            return {
+                todos: sortArrByName
+            };
 
     }
 }
 
-function returnResultSort(sortArr) {
-    return {
-        todos: sortArr
-    };
-}
 
 
 displayTodos(todos);
